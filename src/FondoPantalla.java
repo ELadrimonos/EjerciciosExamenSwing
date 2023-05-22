4c583ce2f26b5092673cc90c9ee7f4e94ade71e4
Adrián Primo <adrianprimob@gmail.com>
2023-05-22 19:08:01 +0200

    Arreglado la imagen de fondo de pantalla

--- src/FondoPantalla.java
+++ src/FondoPantalla.java
@@ -6,7 +6,8 @@ public class FondoPantalla extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
-        Image fondo = new ImageIcon("./pinguino.jpg").getImage();
+        // La ruta relativa de las imagenes empiaza por la raiz del proyecto
+        Image fondo = new ImageIcon("./src/pinguino.jpg").getImage();
         setOpaque(true);
         g2d.drawImage(fondo, 0, 0, 1024, 500,this);
         g2d.drawString("Hola", 2, 34);
